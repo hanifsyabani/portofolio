35aa9803dbca7c95510993e219a6289886008653
hanif <mh070940@gmail.com>
2026-03-28 06:03:39 +0700

    feat: add chat room page with its component and internationalization support.

---
+++ components/chat-room/chat-room.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Breakline from "../ui/breakline";
+import SectionHeading from "../ui/section-heading";
+import SectionSubHeading from "../ui/section-sub-heading";
+import { PiChatTeardropText } from "react-icons/pi";
+import { useTranslations } from "next-intl";
+
+export default function ChatRoom() {
+    const t = useTranslations("ChatRoomPage");
+
+    return (
+        <section>
+            <SectionHeading title={t("title")} icon={<PiChatTeardropText />} className="mb-2" />
+            <SectionSubHeading >
+                {t("description")}
+            </SectionSubHeading>
+
+            <div className="my-10">
+                <Breakline />
+            </div>
+        </section>
+    )
+}
