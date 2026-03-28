3a018bac477ec1d6ebf602e5370c462fcc34ebe6
hanif <mh070940@gmail.com>
2026-03-28 10:16:31 +0700

    feat: integrate Supabase authentication and add locale toggle component

--- components/chat-room/chat-room.tsx
+++ components/chat-room/chat-room.tsx
@@ -5,6 +5,7 @@ import SectionHeading from "../ui/section-heading";
 import SectionSubHeading from "../ui/section-sub-heading";
 import { PiChatTeardropText } from "react-icons/pi";
 import { useTranslations } from "next-intl";
+import AuthButton from "../auth/auth-button";
 
 export default function ChatRoom() {
     const t = useTranslations("ChatRoomPage");
@@ -19,6 +20,8 @@ export default function ChatRoom() {
             <div className="my-10">
                 <Breakline />
             </div>
+
+            <AuthButton />
         </section>
     )
 }
